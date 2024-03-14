a227f686098647672bcd84e4f6487f97814cccf7
samm002 <samuelsdjodis@gmail.com>
2024-03-14 17:22:37 +0800

    Testing LDR and PIR Sensor with Timestamp Done

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,17 @@
 #include <Arduino.h>
 #include "WifiSetup.h"
 #include "NTPSetup.h"
-#define BAUD_RATE 115200
+#include "Credentials.h"
 
-const char* ssid = "Zetta_House";
-const char* password = "ZettaHouseBali2016";
-String currentTimeStamp;
+#define BAUD_RATE 115200
+#define LDR_SENSOR_ANALOG 34
+#define LDR_SENSOR_DIGITAL 32
+#define PIR_SENSOR 35
 
 Wifi wifi(ssid, password);
 TimeStamp timeStamp;
 
+String currentTimeStamp;
 String currentDateStamp;
 String timeStampNow;
 
@@ -17,11 +19,17 @@ void setup() {
   Serial.begin(BAUD_RATE);
   wifi.setup();
   timeStamp.setup();
+  pinMode(LDR_SENSOR_ANALOG, INPUT);
+  pinMode(LDR_SENSOR_DIGITAL, INPUT);
+  pinMode(PIR_SENSOR, INPUT);
 }
 
 void loop() {
   timeStamp.updateTime();
   timeStampNow = timeStamp.getTimeStamp();
   Serial.println(timeStampNow);
+  Serial.println(digitalRead(PIR_SENSOR));
+  Serial.println(digitalRead(LDR_SENSOR_DIGITAL));
+  Serial.println(analogRead(LDR_SENSOR_ANALOG));
   delay(2000);
 }
